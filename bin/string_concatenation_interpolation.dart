e5ee9419dc212a20850ad80dc9512330b2622214
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-07-07 10:16:40 +0200

    string_concatenation_interpolation

---
+++ bin/string_concatenation_interpolation.dart
@@ -0,0 +1,18 @@
+void main() {
+  String name = 'Andre';
+  int age = 42;
+  double height = 1.89;
+  bool likesDart = true;
+  //print('My name is $name. I am $age years old, and i am $height\m tall');
+
+  //String interpolation exercise
+
+  double temperature = 20;
+  int value = 3;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+
+  print('The temperature is ${temperature}C');
+  print('$value + $value = ${value + value}');
+  print('I like $pizza and $pasta');
+}
