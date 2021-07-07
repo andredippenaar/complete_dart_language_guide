7f0125d0432fc7e3d2e54bed135c202857a36309
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-07-07 10:35:10 +0200

    multiline_strings

---
+++ bin/multiline_strings.dart
@@ -0,0 +1,13 @@
+void main() {
+  print('This is a short sentence.\n'
+      'This sentence is a bit longer.\n'
+      'And this very long sentence is the longest of them all. \n');
+
+  //or you could use """
+
+  print('''
+This is a short sentence.
+This sentence is a bit longer.
+And this very long sentence is the longest of them all.
+''');
+}
