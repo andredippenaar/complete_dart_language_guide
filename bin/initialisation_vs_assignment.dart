45812574b7ccfb0a93fdf9e2731136b83943c896
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-07-08 10:57:06 +0200

    initialisation_vs_assignment

---
+++ bin/initialisation_vs_assignment.dart
@@ -0,0 +1,8 @@
+void main() {
+  //ititialisation
+  String title = 'Dart Course';
+  print(title);
+  //assignment - taking a variable that is already declared and assigning a new value to it
+  title = title.toUpperCase();
+  print(title);
+}
---
+++ bin/uppercase_lowercase.dart
@@ -0,0 +1,6 @@
+void main() {
+  var title = 'Dart course';
+
+  print(title.toUpperCase());
+  print(title.toLowerCase());
+}
